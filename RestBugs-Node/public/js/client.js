0a39d57fb4897024252d07341cf09695ab95c306
Gustaf Nilsson Kotte <gustaf.nk@jayway.com>
2013-01-27 23:13:12 +0100

    Extract function loadBugs

--- RestBugs-Node/public/js/client.js
+++ RestBugs-Node/public/js/client.js
@@ -35,71 +35,73 @@ function MyController($scope, $http) {
       $http.get($(link).attr("href")).success(function(data){                  
         
         var xmlDoc = parser.parseFromString(data,"text/xml");
-        var bugsToParse = $(".all li", $(xmlDoc.getElementById("bugs")));
-
-        var bugs = _.map(bugsToParse, function(bugToParse){
-          
-          var actions = _.map($("form", bugToParse), function(form){
-          
-            return {
-              action: $(form).attr("action"),
-              nextCategory: $(form).attr("class").split(" ")[1], // TODO Make more robust
-              method: $(form).attr("method"),
-              id: id = $("input[name=id]", form).attr("value"),
-              name: $("input[name=submit]", form).attr("value"),
-            };
-          });
-
-          return {
-            title: $(".title", bugToParse).text(),
-            description: $(".description", bugToParse).text(),
-            actions: actions            
-          };
-        });
-
-        var viewModel = {
-          name: $(link).attr("rel"), 
-          bugs: bugs, 
-          order: index
-        };
-        
-        var indexForCategory = $scope.getIndexForCategory(viewModel.name);        
-        if (indexForCategory === -1)
-          $scope.categories.push(viewModel);
-        else
-          $scope.categories[indexForCategory] = viewModel;
-
-        _.defer(function(){
-          $(".bug").draggable({ snap: ".droparea" });
-          $(".droparea").droppable({drop: function(event, ui){
-
-            var targetClass = $(this).parent(".category").attr("class");
-            var targetCategory = targetClass.split(" ")[1];
-
-            var draggable = $(ui.draggable);
-            var button = $("button." + targetCategory, draggable);            
-
-            if (button.length === 0) {
-              $scope.loadAndRender();
-              return;
-            }             
-
-            var sourceClass = button.parents(".category").attr("class");
-            var sourceCategory = sourceClass.split(" ")[1];
-
-            var action = {
-              action: button.data("action"),
-              nextCategory: targetCategory,
-              method: button.data("method"),
-              id: id = button.data("id")
-            };
-
-            $scope.move({currentCategory: sourceCategory, action: action});            
-          }});  
-        });
-        
+        $scope.loadBugs(xmlDoc, link, index);
       });      
     });   
+  }
+
+  $scope.loadBugs = function(xmlDoc, link, index){
+    var bugsToParse = $(".all li", $(xmlDoc.getElementById("bugs")));
+    var bugs = _.map(bugsToParse, function(bugToParse){
+          
+      var actions = _.map($("form", bugToParse), function(form){
+      
+        return {
+          action: $(form).attr("action"),
+          nextCategory: $(form).attr("class").split(" ")[1], // TODO Make more robust
+          method: $(form).attr("method"),
+          id: id = $("input[name=id]", form).attr("value"),
+          name: $("input[name=submit]", form).attr("value"),
+        };
+      });
+
+      return {
+        title: $(".title", bugToParse).text(),
+        description: $(".description", bugToParse).text(),
+        actions: actions            
+      };
+    });
+
+    var viewModel = {
+      name: $(link).attr("rel"), 
+      bugs: bugs, 
+      order: index
+    };
+    
+    var indexForCategory = $scope.getIndexForCategory(viewModel.name);        
+    if (indexForCategory === -1)
+      $scope.categories.push(viewModel);
+    else
+      $scope.categories[indexForCategory] = viewModel;
+
+    _.defer(function(){
+      $(".bug").draggable({ snap: ".droparea" });
+      $(".droparea").droppable({drop: function(event, ui){
+
+        var targetClass = $(this).parent(".category").attr("class");
+        var targetCategory = targetClass.split(" ")[1];
+
+        var draggable = $(ui.draggable);
+        var button = $("button." + targetCategory, draggable);            
+
+        if (button.length === 0) {
+          $scope.loadAndRender();
+          return;
+        }             
+
+        var sourceClass = button.parents(".category").attr("class");
+        var sourceCategory = sourceClass.split(" ")[1];
+
+        var action = {
+          action: button.data("action"),
+          nextCategory: targetCategory,
+          method: button.data("method"),
+          id: id = button.data("id")
+        };
+
+        $scope.move({currentCategory: sourceCategory, action: action});            
+      }});  
+    });  
   }
 
   var parser = new DOMParser();
