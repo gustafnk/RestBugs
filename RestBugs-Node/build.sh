3ad8da358afed0874c90862333a0fb0baec74b7f
Gustaf Nilsson Kotte <gustaf.nk@jayway.com>
2013-02-02 15:18:35 +0100

    Moved external js files to lib

--- RestBugs-Node/build.sh
+++ RestBugs-Node/build.sh
@@ -1,3 +1,3 @@
        
-cat "public/js/jquery-ui-1.10.0.custom.min.js" "public/js/underscore-min.js" "public/js/bootstrap.min.js" "public/js/angular.min.js" "public/js/client.js" > "public/js/client.min.js"
+cat "public/js/lib/jquery-ui-1.10.0.custom.min.js" "public/js/lib/underscore-min.js" "public/js/lib/bootstrap.min.js" "public/js/lib/angular.min.js" "public/js/client.js" > "public/js/client.min.js"
 cat "public/css/bootstrap.min.css" "public/css/bootstrap-responsive.min.css" "public/css/client.css" > "public/css/client.min.css"
