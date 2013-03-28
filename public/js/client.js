47e6fefb4af014425299363e4b250d6c461125bd
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2013-03-28 10:53:52 +0100

    Fix add form bug

--- public/js/client.js
+++ public/js/client.js
@@ -8,24 +8,26 @@ function BoardController($scope, $http) {
 
     $scope.rels = _.map(restbugs.init.links, function(link){ // Move to load
       return {
-        rel: $(link).attr("rel"), 
+        rel: $(link).attr("rel"),
         href: $(link).attr("href")
       };
     });
 
     $scope.load();
 
-    $(".addFormContainer").html(restbugs.init.addForm);
-
+    if (restbugs.init.addForm && restbugs.init.addForm.length !== 0)
+      $(".addFormContainer").html(restbugs.init.addForm);
+    else //tmp fix
+      window.location.replace("/");
 
     // Hijax the add form container
     $("form.new").submit(function(){
-      
-      var $that = $(this);      
+
+      var $that = $(this);
       $.post($(this).attr("action"), $(this).serialize()).done(function(){
-        
+
         $scope.load();
-        
+
         // Empty the input fields
         $(".addFormContainer").hide();
         _.each($("input[type!=submit]", $that.parent()), function(item){
@@ -38,53 +40,53 @@ function BoardController($scope, $http) {
   }
 
   $scope.load = function(){
-          
+
     var rels = $scope.rels;
-    
-    var numberOfCallbacks = 0;    
+
+    var numberOfCallbacks = 0;
     _.each(rels, function(link, index){
-      
-      $http.get($(link).attr("href")).success(function(data){                  
+
+      $http.get($(link).attr("href")).success(function(data){
         numberOfCallbacks += 1;
 
         var bugsDiv = $(data).filter(function(){ return $(this).attr("id") === "bugs" });
-      
+
         $scope.loadBugs(bugsDiv, link, index);
 
         // Since jQuery promises is broken by design - or that I don't understand them properly
-        if (numberOfCallbacks === rels.length) {          
+        if (numberOfCallbacks === rels.length) {
           _.defer($scope.bindActions);
           _.defer($scope.enableDragAndDrop);
         }
 
-      });      
-    });  
-  
+      });
+    });
+
   }
 
   $scope.loadBugs = function(bugsDiv, link, index){
-    
+
     var bugs = restbugs.domain.getBugs(bugsDiv);
 
     var viewModel = {
-      name: $(link).attr("rel"), 
-      bugs: bugs, 
+      name: $(link).attr("rel"),
+      bugs: bugs,
       order: index
     };
-    
-    var indexForCategory = $scope.getIndexForCategory(viewModel.name);        
+
+    var indexForCategory = $scope.getIndexForCategory(viewModel.name);
     if (indexForCategory === -1)
       $scope.categories.push(viewModel);
     else
-      $scope.categories[indexForCategory] = viewModel;  
+      $scope.categories[indexForCategory] = viewModel;
   }
 
   $scope.bindActions = function(){
-    
+
     // Hijax the move forms
-    $("form.move").submit(function(){                  
-      $.post($(this).attr("action"), $(this).serialize()).done(function(){        
-        $scope.load();                
+    $("form.move").submit(function(){
+      $.post($(this).attr("action"), $(this).serialize()).done(function(){
+        $scope.load();
       });
 
       return false;
@@ -93,7 +95,7 @@ function BoardController($scope, $http) {
 
   $scope.move = function(args){
     var action = args.action;
-    
+
     if (action.method === "POST") {
       $http.post(action.action, {id: action.id}).success(function(data) {
         $scope.load();
@@ -116,7 +118,7 @@ function BoardController($scope, $http) {
   $scope.enableDragAndDrop = function(){
 
     var onDropped = function(event, ui){
-      
+
       var targetClass = $(this).parent(".category").attr("class");
       var targetCategory = targetClass.split(" ")[1];
 
@@ -125,10 +127,10 @@ function BoardController($scope, $http) {
       var action = $("form." + targetCategory + " input[type=submit]", draggable);
 
       if (action.length !== 0)
-        action.click();        
+        action.click();
       else
         $scope.load(); // TODO Also show notification message?
-    
+
     };
 
     $(".bug").draggable({ snap: ".droparea" });
@@ -144,12 +146,12 @@ restbugs.init = restbugs.init || (function() {
 
   var module = {
     load : function (){
-      this.links = $("a[rel!=index]");      
+      this.links = $("a[rel!=index]");
       this.addForm = $("form.new");
 
       // Load template and bootstrap angular
-      $.ajax({url: "/templates.html"}).done(function(templates){  
-   
+      $.ajax({url: "/templates.html"}).done(function(templates){
+
         var template = $("#angular-template", $(templates)).text();
 
         $("body").html(template);
@@ -157,11 +159,11 @@ restbugs.init = restbugs.init || (function() {
         $(".addFormContainer").hide();
 
         var containerElement = $('body');
-        angular.bootstrap(containerElement, ['board']); 
+        angular.bootstrap(containerElement, ['board']);
       });
     }
-  }    
-      
+  }
+
   return module;
 }());
 
@@ -174,14 +176,14 @@ restbugs.domain = restbugs.dodmain || (function() {
       var bugsToParse = $(".all li", bugsDiv);
 
       var parseBug = function(bugToParse){
-                                    
+
         var formsHtml = _.map($("form", bugToParse).clone().wrap('<p>').parent(), function(item){
           return $(item).html()
         }).join("\n");
 
         return {
           title: $(".title", bugToParse).text(),
-          description: $(".description", bugToParse).text(),          
+          description: $(".description", bugToParse).text(),
           forms: formsHtml
         };
     };
@@ -189,8 +191,8 @@ restbugs.domain = restbugs.dodmain || (function() {
     var bugs = _.map(bugsToParse, parseBug);
     return bugs;
     }
-  }    
-      
+  }
+
   return module;
 }());
 
